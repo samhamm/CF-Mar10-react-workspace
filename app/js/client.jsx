bc49a68d6bbab98e05adbe387478442e007fc428
samhamm <samhamm@gmail.com>
2015-03-11 14:58:22 -0700

    pre jquery

--- app/js/client.jsx
+++ app/js/client.jsx
@@ -3,8 +3,29 @@
 'use strict';
 
 var React = require('react');
+var ajax = require('jquery').ajax;
 
 var notesData = [{noteBody: 'hello world', _id: 1 }, {noteBody: 'goodbye world', _id: 2}];
+
+var NoteForm = React.createClass({
+  getInitialState: function() {
+    return {newNote: {noteBody: ''}};
+  },
+
+  handleChange: function(event) {
+   this.setState({newNote: {noteBody: event.target.value}});
+  },
+
+  render: function() {
+    return (
+      <form>
+        <label htmlFor="newnote">New Note</label>
+        <input id="newnote" type="text" value={this.state.newNote.notesBody} onChange={this.handleChange} />
+        <button type="submit">Create New Note</button>
+      </form>
+    )
+  }
+});
 
 var Note = React.createClass({
   render: function() {
@@ -31,6 +52,13 @@ var NoteList = React.createClass({
 var App = React.createClass({
   getInitialState: function() { //puts it in state, instead of hard-coded into props
     return {notesData: notesData};
+  },
+
+  onNewNote: function(note) {
+    note._id = this.state.notesData.length + 1;
+    var notesDataCopy = this.state.notesData;
+    notesDataCopy.push(note);
+    this.setSTate({notesData: {notesDataCopy}});
   },
 
   render: function () {    //will return JS for rendering an element, or JSX
